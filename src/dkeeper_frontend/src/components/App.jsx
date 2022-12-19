0487dd0428065e1cb3403d4523997b3194c64484
ratikantmohanta <ratikantmohanta@yahoo.com>
2022-12-19 10:42:49 +0530

    C1 Comleted

---
+++ src/dkeeper_frontend/src/components/App.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import { dkeeper_backend } from "../../../declarations/dkeeper_backend"
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    console.log("triggered");
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const notesArray = await dkeeper_backend.readNotes();
+    setNotes(notesArray);
+  }
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      dkeeper_backend.createNote(newNote.title, newNote.content)
+      return [newNote, ...prevNotes];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      dkeeper_backend.removeNote(id);
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
